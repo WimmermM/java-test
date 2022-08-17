e24e88abb3f5a6097e1c72f9e4c0da2872cf2ab9
WimmermM <marek.wimmer@gmail.com>
2022-08-17 18:15:29 +0200

    initial commit

---
+++ techband-test/src/main/java/com/example/techbandtest/TechbandTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.techbandtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbandTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbandTestApplication.class, args);
+	}
+
+}
---
+++ techband-test/src/test/java/com/example/techbandtest/TechbandTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techbandtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbandTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
